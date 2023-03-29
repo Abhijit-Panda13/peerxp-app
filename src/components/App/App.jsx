1e2424526519d595e01c30bcb3021eb5e1b8f0d8
Abhijit <abhijit.panda1319@gmail.com>
2023-03-29 11:03:42 +0530

    Setup Routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, {useState} from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from '../Dashboard/Dashboard';
+import Login from '../Login/Login';
 import Preferences from '../Preferences/Preferences';
 
 function App() {
+  const [token, setToken] = useState();
+  if(!token) {
+      return <Login setToken={setToken} />
+  }
   return (
-    <></>
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/dashboard" element={<Dashboard />}/>
+          <Route exact path="/preferences" element={<Preferences />}/>  
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
